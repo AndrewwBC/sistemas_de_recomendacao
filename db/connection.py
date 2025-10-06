48e927b136535bf51be93ad5967beab8a9e26d02
AndrewwBC <andrew-hot2011@hotmail.com>
2025-10-06 14:36:32 -0300

    add env

--- db/connection.py
+++ db/connection.py
@@ -1,10 +1,20 @@
 import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+db_user = os.getenv("DB_USER")
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_pass = os.getenv("DB_PASS")
 
 def openConnection():
     return psycopg2.connect(database = "psr", 
-    user = "postgres", 
-    host= 'localhost',
-    password = "1234",
-    port = 5432)
+    user = db_user, 
+    host= db_host,
+    password = db_pass,
+    port = db_port)
     
 db = openConnection()
+print(db)
